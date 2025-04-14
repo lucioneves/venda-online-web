b425635cdc12fc5dc2b631026b57817583e6edf7
Lucio Neves <luciocecilia20@gmail.com>
2025-04-14 17:24:52 -0400

    feat: create module login

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import { BackgroundImage } from "../styles/LoginScreen.styles";
+
+
+const LoginScreen = () => {
+  return (
+    <div>
+       <BackgroundImage src="./background.png" />
+    </div>
+  );
+};
+
+export default LoginScreen;
