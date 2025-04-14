04d8007b0cf10a4af4a696a71eebbf597b3de7ec
Lucio Neves <luciocecilia20@gmail.com>
2025-04-14 15:37:24 -0400

    feat: insert antd and styled-componentes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <StyledLink>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -24,8 +25,13 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </StyledLink>
   );
 }
 
+const StyledLink = styled.div`
+color: blue;
+  font-size: 20px;
+`;
 export default App;
+
