9561de5d7714dd3d6a8162455e7022ee89a09e23
Lucio Neves <luciocecilia20@gmail.com>
2025-04-22 11:46:18 -0400

    feat: change layout and create button component

--- src/shared/buttons/button/Button.tsx
+++ src/shared/buttons/button/Button.tsx
@@ -1,13 +1,13 @@
-import { ButtonProps } from "antd";
+import { ButtonProps } from 'antd';
 
-import { ButtonAntd } from "./button.styles";
+import { ButtonAntd } from './button.styles';
 
 interface ButtonCurrentProps extends ButtonProps {
   margin?: string;
 }
 
 const Button = ({ margin, ...props }: ButtonCurrentProps) => {
-  return <ButtonAntd style={{ margin }} {...props }/>;  
+  return <ButtonAntd style={{ margin }} {...props} />;
 };
 
 export default Button;
